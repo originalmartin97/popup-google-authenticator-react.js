f15a58418070f1bb5c1b2144ad7940f818677077
Takács Martin <originalmartin97@gmail.com>
2024-04-03 19:04:38 +0200

    started over

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1Wb-tpASrMG3WP3tRq_ZPCFkUsR5NSsg",
+  authDomain: "chat-demo-3d4d0.firebaseapp.com",
+  projectId: "chat-demo-3d4d0",
+  storageBucket: "chat-demo-3d4d0.appspot.com",
+  messagingSenderId: "1028414620426",
+  appId: "1:1028414620426:web:c71d2ef9775d63f43e70f3",
+  measurementId: "G-YN4WFBQ67F"
+};
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
